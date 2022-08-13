194cdc126eddad672499fd44151aa838752bd261
bjcore <b.jun.kim@outlook.com>
2022-08-13 21:39:50 +0900

    update

--- p5/1-self-portrait/sketch.js
+++ p5/1-self-portrait/sketch.js
@@ -5,18 +5,22 @@ function setup() {
 
 function draw() {
   strokeWeight(4);
-  triangle(250, 350, 100, 350, 200, 150);
-  triangle(400, 350, 250, 350, 300, 150);
-  fill(255);
+  triangle(250, 340, 110, 340, 200, 120);
+  triangle(390, 340, 250, 340, 300, 120);
   ellipseMode(CENTER);
-  ellipse(250, 600, 500, 500);
+  ellipse(250, 220, 220, 230);
+  fill(255);
+  ellipse(150, 230, 30, 40);
+  ellipse(350, 230, 30, 40);
+  ellipse(250, 600, 470, 500);
   rectMode(CENTER);
-  rect(250, 350, 50, 75);
+  rect(250, 350, 70, 80);
   fill(255);
   ellipse(250, 225, 200, 225);
   fill(0);
-  ellipse(210, 220, 10, 10);
-  ellipse(290, 220, 10, 10);
+  ellipse(210, 225, 10, 15);
+  ellipse(290, 225, 10, 15);
   ellipseMode(RADIUS);
-  arc(250, 255, 50, 50, 0, PI, CHORD);
+  arc(250, 265, 50, 50, 0, PI, CHORD);
+  arc(250, 200, 97, 97, PI, 0, CHORD);
 }
